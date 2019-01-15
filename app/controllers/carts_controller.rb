d4d47e8d92847ec16fba55e026accbfb4535959a
vivek3894 <viveknagaraju@yahoo.in>
2019-01-15 16:27:32 +0530

    indent code

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,46 +1,46 @@
 class CartsController < ApplicationController
-    
-    before_action :delete_cart_products_cache , only: [:complete_cart]
 
-	def add_products_to_cart
-		product_present = redis.sismember(redis_key , params[:product_id])
-		if product_present
-			message = "Product already Present in your cart!"
-		else
-		   @order = current_cart.orders.build(product_id: params[:product_id])
-		   @order.save
-           message = "Product successfully added to your cart!"
-           redis.sadd(redis_key , params[:product_id])
-	    end
-	    render json: {message: message , status: :ok}
-	end
+  before_action :delete_cart_products_cache , only: [:complete_cart]
 
-	def complete_cart
-        @purchasable_products = current_cart.products.where('inventory_count > ?' , 0)
-        @purchasable_products.update_all("inventory_count = inventory_count-1")
-        current_cart.set_active_status
-        render json: { message: "All products with inventory > 0 have been purchased" ,
-        products_purchased: @purchasable_products ,
-        status: :ok }
-	end
-
-	def get_cart_details
-		@all_products = current_cart.products
-		render json: {product_details: @all_products , Total: @all_products.sum('price') , status: :ok}
-	end
-    
-    private
-
-    def redis
-        $redis
+  def add_products_to_cart
+    product_present = redis.sismember(redis_key , params[:product_id])
+    if product_present
+      message = "Product already Present in your cart!"
+    else
+      @order = current_cart.orders.build(product_id: params[:product_id])
+      @order.save
+      message = "Product successfully added to your cart!"
+      redis.sadd(redis_key , params[:product_id])
     end
+    render json: {message: message , status: :ok}
+  end
 
-    def redis_key
-    	"cart_products:#{current_cart.id}"
-    end
+  def complete_cart
+    @purchasable_products = current_cart.products.where('inventory_count > ?' , 0)
+    @purchasable_products.update_all("inventory_count = inventory_count-1")
+    current_cart.set_active_status
+    render json: { message: "All products with inventory > 0 have been purchased" ,
+                   products_purchased: @purchasable_products ,
+                   status: :ok }
+  end
 
-    def delete_cart_products_cache
-    	redis.del(redis_key)
-    end
+  def get_cart_details
+    @all_products = current_cart.products
+    render json: {product_details: @all_products , Total: @all_products.sum('price') , status: :ok}
+  end
+
+  private
+
+  def redis
+    $redis
+  end
+
+  def redis_key
+    "cart_products:#{current_cart.id}"
+  end
+
+  def delete_cart_products_cache
+    redis.del(redis_key)
+  end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
-   
-   def all_product_details
-   	@all_products = Product.all
-   	@all_products = @all_products.select{|x| x.inventory_count > 0} if params["is_inventory"] == "true"
-   	render json: @all_products , status: :ok
-   end
+
+  def all_product_details
+    @all_products = Product.all
+    @all_products = @all_products.select{|x| x.inventory_count > 0} if params["is_inventory"] == "true"
+    render json: @all_products , status: :ok
+  end
 
 end
